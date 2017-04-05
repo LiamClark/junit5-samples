a82d13fce88c455eefd4b9a071fe519a09731250
Liam Clark <L.A.Clark@student.tudelft.nl>
2017-04-05 15:31:57 +0200

    Move test files into sample project.
    The gradle output remains the same:
    
    Test run finished after 22 ms
    [         2 containers found      ]
    [         0 containers skipped    ]
    [         2 containers started    ]
    [         0 containers aborted    ]
    [         2 containers successful ]
    [         0 containers failed     ]
    [         0 tests found           ]
    [         0 tests skipped         ]
    [         0 tests started         ]
    [         0 tests aborted         ]
    [         0 tests successful      ]
    [         0 tests failed          ]

---
+++ junit5-gradle-consumer/src/test/java/com/example/project/BugTest.java
@@ -0,0 +1,15 @@
+package com.example.project;
+
+
+import org.junit.Test;
+
+public abstract class BugTest {
+
+    public BugTest(int x) {
+
+    }
+
+    @Test
+    public void test() {}
+
+}
---
+++ junit5-gradle-consumer/src/test/java/com/example/project/SubBugTest.java
@@ -0,0 +1,7 @@
+package com.example.project;
+
+class SubBugTest extends BugTest {
+    public SubBugTest() {
+        super(2);
+    }
+}
